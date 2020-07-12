bd7ad76d86ca6d15883b0bbba96c4d0c4adce774
FJReyna <franciscorgs@hotmail.es>
2020-07-12 15:13:34 -0500

    added host env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ const env = require('./env.js');
  
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.DATABASE_URL,
+  host: env.host,
   dialect: env.dialect,
   operatorsAliases: false,
  
